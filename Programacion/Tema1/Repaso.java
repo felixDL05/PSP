e2d6098a36a1a41974332d0e3d5e28b7c2b8889c
Felix <felixdominguezlopez@gmail.com>
2026-09-09 20:32:15 +0200

    Ejercicio Java

---
+++ Programacion/Tema1/Repaso.java
@@ -0,0 +1,15 @@
+package com.mycompany.repaso;
+
+public class Repaso {
+
+    public static int sumaNumeros(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int sn = sumaNumeros(5, 5);
+        System.out.println(sn);
+    }
+}
